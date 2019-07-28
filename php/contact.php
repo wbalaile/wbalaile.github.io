55666f736059154f0d12502eb245ac85d9ee4f60
wilby_mj <snoop.monotone@gmail.com>
2019-07-29 01:08:48 +0300

    a

--- php/contact.php
+++ php/contact.php
@@ -7,7 +7,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 $formcontent = "From: $name \n Message: $message ";
-$recipient = "info@kiganjani.com";
+$recipient = "wbalaile@live.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
